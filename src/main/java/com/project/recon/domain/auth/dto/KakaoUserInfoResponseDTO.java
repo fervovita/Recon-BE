9cb667ce10dba694ec63ae24de447d1d061b9b51
Junseok Ahn <rrvvy9595@gmail.com>
2026-02-12 00:50:30 +0900

    #3 feat: 카카오 토큰 발급 로직 구현

---
+++ src/main/java/com/project/recon/domain/auth/dto/KakaoUserInfoResponseDTO.java
@@ -0,0 +1,38 @@
+package com.project.recon.domain.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Getter
+@NoArgsConstructor
+public class KakaoUserInfoResponseDTO {
+
+    private Long id;
+
+    @JsonProperty("kakao_account")
+    private KakaoAccount kakaoAccount;
+
+    @Getter
+    @NoArgsConstructor
+    public static class KakaoAccount {
+        private String email;
+        private Profile profile;
+
+        @Getter
+        @NoArgsConstructor
+        public static class Profile {
+            private String nickname;
+        }
+    }
+
+
+    public String getNickname() {
+        return kakaoAccount.getProfile().getNickname();
+    }
+
+    public String getEmail() {
+        return kakaoAccount.getEmail();
+    }
+}
