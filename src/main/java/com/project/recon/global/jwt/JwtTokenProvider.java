d0cd94569adb1e847c0c4171db5217a08f018780
Junseok Ahn <rrvvy9595@gmail.com>
2026-02-12 00:20:59 +0900

    #3 feat: JWT 설정 추가

---
+++ src/main/java/com/project/recon/global/jwt/JwtTokenProvider.java
@@ -0,0 +1,90 @@
+package com.project.recon.global.jwt;
+
+import com.project.recon.domain.user.entity.User;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.access-token-expiration}")
+    private Long accessTokenExpiration;
+
+    @Value("${jwt.refresh-token-expiration}")
+    private Long refreshTokenExpiration;
+
+    public SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateAccessToken(User user) {
+        return generateToken(user, accessTokenExpiration, TokenType.ACCESS);
+    }
+
+    public String generateRefreshToken(User user) {
+        return generateToken(user, refreshTokenExpiration, TokenType.REFRESH);
+    }
+
+
+    private String generateToken(User user, long expiresIn, TokenType tokenType) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiresIn);
+
+        return Jwts.builder()
+                .setSubject(user.getId().toString())
+                .setExpiration(expiryDate)
+                .claim("tokenType", tokenType)
+                .setIssuedAt(now)
+                .signWith(getSecretKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey())
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (ExpiredJwtException e) {
+            log.error("만료된 JWT 토큰: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("지원되지 않는 JWT 토큰: {}", e.getMessage());
+        } catch (MalformedJwtException e) {
+            log.error("잘못된 형식의 JWT 토큰: {}", e.getMessage());
+        } catch (SignatureException e) {
+            log.error("JWT 서명 검증 실패: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT 토큰이 비어있음: {}", e.getMessage());
+        }
+        return false;
+    }
+
+    public Long getUserIdFromToken(String token) {
+        try {
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey((getSecretKey()))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
+
+            return Long.parseLong(claims.getSubject());
+        } catch (JwtException e) {
+            log.error("토큰에서 사용자 Id 추출 실패 : {}", e.getMessage());
+            return null;
+        }
+    }
+
+}
---
+++ src/main/java/com/project/recon/global/jwt/TokenType.java
@@ -0,0 +1,5 @@
+package com.project.recon.global.jwt;
+
+public enum TokenType {
+    ACCESS, REFRESH
+}
