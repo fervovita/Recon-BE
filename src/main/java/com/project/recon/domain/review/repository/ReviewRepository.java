479d03386039d42e95371f6d9359d41329bfd654
Junseok Ahn <rrvvy9595@gmail.com>
2026-02-18 01:14:48 +0900

    #26 fix: 후기 목록 조회시 N+1 문제 해결

--- src/main/java/com/project/recon/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/project/recon/domain/review/repository/ReviewRepository.java
@@ -3,11 +3,13 @@ package com.project.recon.domain.review.repository;
 import com.project.recon.domain.review.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     boolean existsByProductIdAndUserId(Long productId, Long userId);
 
+    @EntityGraph(attributePaths = {"user"})
     Page<Review> findByProductId(Long productId, Pageable pageable);
 }
