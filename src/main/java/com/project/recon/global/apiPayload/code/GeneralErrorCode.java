a2b21724a4c1a516025afcc01e1cc62c0b8f7d36
Junseok Ahn <rrvvy9595@gmail.com>
2026-02-18 19:28:56 +0900

    #28 feat: 데이터베이스 제약 조건 위반 예외 처리 로직 추가

--- src/main/java/com/project/recon/global/apiPayload/code/GeneralErrorCode.java
+++ src/main/java/com/project/recon/global/apiPayload/code/GeneralErrorCode.java
@@ -68,6 +68,7 @@ public enum GeneralErrorCode implements BaseErrorCode {
     INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "REQ_4002", "파라미터 형식이 잘못되었습니다."),
     INVALID_BODY_TYPE(HttpStatus.BAD_REQUEST, "REQ_4003", "요청 본문의 형식이 잘못되었거나, 허용되지 않은 값이 포함되어 있습니다."),
     UNSUPPORTED_CONTENT_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "REQ_4151", "지원하지 않는 Content-Type입니다."),
+    DUPLICATE_RESOURCE(HttpStatus.CONFLICT, "REQ_4091", "이미 존재하는 데이터입니다."),
 
     // API/라우팅 에러
     API_NOT_FOUND(HttpStatus.NOT_FOUND, "API_4041", "존재하지 않는 API입니다."),
