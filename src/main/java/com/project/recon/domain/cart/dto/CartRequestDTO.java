adb2c61d8301757c46aa3423bd7f3db63df0732b
Junseok Ahn <rrvvy9595@gmail.com>
2026-02-20 14:40:40 +0900

    #32 fix: quantity를 Integer로 변경

--- src/main/java/com/project/recon/domain/cart/dto/CartRequestDTO.java
+++ src/main/java/com/project/recon/domain/cart/dto/CartRequestDTO.java
@@ -12,7 +12,8 @@ public class CartRequestDTO {
         @NotNull(message = "상품 ID는 필수입니다.")
         private Long productId;
 
+        @NotNull(message = "수량이 없습니다.")
         @Min(value = 1, message = "수량은 1 이상이어야 합니다.")
-        private int quantity;
+        private Integer quantity;
     }
 }
