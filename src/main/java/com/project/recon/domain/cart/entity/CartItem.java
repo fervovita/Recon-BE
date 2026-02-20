61d8a9464990dcfadff01351552a226a4ab6369e
Junseok Ahn <rrvvy9595@gmail.com>
2026-02-20 13:02:19 +0900

    #32 feat: 장바구니 Entity 추가

---
+++ src/main/java/com/project/recon/domain/cart/entity/CartItem.java
@@ -0,0 +1,44 @@
+package com.project.recon.domain.cart.entity;
+
+import com.project.recon.domain.product.entity.Product;
+import com.project.recon.global.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"cart_id", "product_id"}))
+public class CartItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+
+    public static CartItem createCartItem(Cart cart, Product product, int quantity) {
+        return CartItem.builder()
+                .cart(cart)
+                .product(product)
+                .quantity(quantity)
+                .build();
+    }
+
+    public void addQuantity(int quantity) {
+        this.quantity += quantity;
+    }
+
+}
