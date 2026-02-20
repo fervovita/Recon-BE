f1b37e2bebb56e9d11ba5357368dac3d26e8959c
Junseok Ahn <rrvvy9595@gmail.com>
2026-02-20 14:48:58 +0900

    #32 refactor: 장바구니 엔드포인트를 /carts로 변경

--- src/main/java/com/project/recon/domain/cart/controller/CartController.java
+++ src/main/java/com/project/recon/domain/cart/controller/CartController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/cart")
+@RequestMapping("/api/v1/carts")
 @Tag(name = "Cart", description = "장바구니 관련 API")
 public class CartController {
 
